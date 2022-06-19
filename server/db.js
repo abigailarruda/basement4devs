d9c56fa429cf697b05a6e795f0341a6339acb894
Abigail Arruda <itssabbie@gmail.com>
2022-06-19 20:21:47 -0300

    Create server with GraphQL and mock data

---
+++ server/db.js
@@ -0,0 +1,206 @@
+module.exports = {
+  terms: [
+    {
+      id: "1731c2e8-28bc-4278-af71-1442ce63c0c9",
+      term: "Ação",
+      descriptions: [
+        "Uma ação é um título que representa a propriedade de uma parcela do capital social de uma empresa. Ela permite que o proprietário das ações tenha direito a uma proporção dos ativos e lucros da corporação igual à quantidade que possui.",
+        "Essa quantidade é determinada da seguinte forma: se uma empresa tem 1.000 ações em circulação e uma pessoa possui 100 ações, essa pessoa possui e tem direito a 10% dos ativos e lucros da empresa.",
+        "As ações são compradas e vendidas predominantemente em bolsas de valores (embora também possa haver vendas privadas). As corporações emitem (vendem) ações para levantar fundos para o negócio, o que pode ser feito sempre que houver necessidade. Esse processo dilui a propriedade e os direitos dos acionistas existentes."
+      ],
+      tag: null
+    },
+    {
+      id: "0fe4a64d-1a69-4fbf-8180-90ab2d8a1e9f",
+      term: "Cap Table",
+      descriptions: ["O cap table é a abreviação de “capitalization table” (em português, “tabela de capitalização”). Trata-se do registro de como as participações atuais e futuras de uma determinada empresa estão divididas entre os seus stakeholders."],
+      tag: null
+    },
+    {
+      id: "177223eb-ca00-45dc-a354-8c27835b4905",
+      term: "Classes de Ativo",
+      descriptions: ["Classes de ativos são grupos de ativos com características semelhantes entre si, e que se comportam de forma similar no mercado. Cada classe de ativo possui características próprias e podem existir diversos veículos para representá-la."],
+      tag: null
+    },
+    {
+      id: "347789ff-59ef-4475-b6ff-e0004884bdc0",
+      term: "Cliff",
+      descriptions: ["O cliff é o período estabelecido pela empresa que oferta stock options até que a primeira parcela do contrato esteja disponível para exercício. Após o cliff, as opções poderão ser adquiridas de forma gradual a cada intervalo."],
+      tag: null
+    },
+    {
+      id: "31908c8a-6509-4dc6-b28f-927e29575215",
+      term: "Debêntures",
+      descriptions: [
+        "Debêntures são títulos de crédito emitidos por empresas e negociados no mercado de capitais. Em alguns aspectos, seu funcionamento lembra o dos títulos públicos negociados no Tesouro Direto. Só que em vez de financiar o governo, quem compra debêntures empresta dinheiro para uma empresa.",
+        "Quem investe em uma debênture já sabe desde o início por quanto tempo o dinheiro precisará ficar aplicado e também de quanto serão os juros que receberá até lá. Por essas características, as debêntures são classificadas como investimentos de renda fixa, como os CDBs."
+      ],
+      tag: null
+    },
+    {
+      id: "0ad3978f-ce18-4aee-8d50-c8410e83ca88",
+      term: "Desdobramento",
+      descriptions: [
+        "O desdobramento ou split é um fato que ocorre com ações ou diferentes tipos de ativos, onde a unidade é dividida e o valor da unidade também.",
+        "Exemplo: uma ação de uma empresa tem o valor de R$ 100,00, então ela passa pelo desdobramento. O desdobramento definido é de 1 para 5, isso significa que cada ação se tornará 5 ações e o valor que antes era de R$ 100,00 por ação, agora será de R$ 20,00 por ação."
+      ],
+      tag: null
+    },
+    {
+      id: "ed3a2eef-b63f-4ec1-974f-708e850e2fb6",
+      term: "Equity",
+      descriptions: [
+        "O equity de uma empresa representa a totalidade das suas propriedades e é um indicador do quão saudável o negócio está em termos financeiros. Através dele pode-se identificar se há uma boa oportunidade de investimento.",
+        "Se uma empresa vendesse todos os seus ativos (propriedades, equipamentos, investimentos, contas a receber, etc) e ao deduzir seus passivos (contas a pagar, dívidas, impostos e empréstimos) o valor restante (equity) for substancial, isso significa que a empresa será uma oportunidade melhor para investidores lucrarem."
+      ],
+      tag: null,
+    },
+    {
+      id: "42ce2642-2210-46b7-85ff-35d3b8a504b4",
+      term: "Fair Value",
+      descriptions: ["Fair Value ou Valor Justo é uma técnica contábil utilizada na determinação dos valores de determinados ativos de uma organização. Seu propósito é apresentar os ativos pelo provável fluxo de caixa exigido ou obtido no mercado."],
+      tag: null
+    },
+    {
+      id: "021519bb-27f8-427d-9194-c6de786af241",
+      term: "Forma Societária",
+      descriptions: ["A forma societária de uma empresa define o seu tratamento jurídico: quais são suas obrigações, leis aplicáveis ao seu caso, a participação ou não de sócios no negócio e a sua parte no capital social. Existem vários tipos de natureza jurídica para um CNPJ."],
+      tag: null
+    },
+    {
+      id: "d3e81c3e-0337-4c2b-9eae-3f4279edf613",
+      term: "Corporation",
+      descriptions: ["Uma Corporation é uma entidade legal separada e distinta de seus proprietários. Elas podem firmar contratos, emprestar e pedir dinheiro emprestado, processar e ser processado, contratar funcionários, possuir bens e pagar impostos. Alguns se referem a uma Corporation como uma “pessoa jurídica”."],
+      tag: "Forma Societária"
+    },
+    {
+      id: "04f9d3be-f2dc-4825-8db5-ca6a91f1e676",
+      term: "EIRELI",
+      descriptions: [
+        "A Empresa Individual de Responsabilidade Limitada (EIRELI) é um tipo societário de microempresa no qual é exigido apenas um sócio, o proprietário. Sem limite de faturamento anual e podendo optar pelo Simples Nacional, essa categoria é uma boa opção para empreendedores e profissionais que não se enquadram como MEI.",
+        "Essa forma societária foi extinta em agosto de 2021. Dessa forma, todas as empresas registradas nessa categoria empresarial foram transformadas automaticamente em Sociedade Limitada Unipessoal (SLU), que possui, essencialmente, as mesmas características da EIRELI, mas não exige capital social mínimo e sócio para abrir empresa e separa o patrimônio pessoal do patrimônio do empreendedor."
+      ],
+      tag: "Forma Societária"
+    },
+    {
+      id: "4dccfdcb-5de6-4aa5-968d-7224e746902b",
+      term: "LLC",
+      descriptions: ["Uma Limited Liability Company (LLC) é uma estrutura de negócios nos EUA que protege seus proprietários da responsabilidade pessoal por suas dívidas ou responsabilidades. Equivale à LTDA no Brasil."],
+      tag: "Forma Societária"
+    },
+    {
+      id: "7f926c43-1d0d-4113-aab9-936c98cfeb4e",
+      term: "LTD",
+      descriptions: ["Uma Private Company Limited by Shares (LTD) é uma forma societária corporativa disponível em países como Reino Unido, Irlanda e Canadá. O termo aparece como um sufixo que segue o nome da empresa, indicando que se trata de uma sociedade por quotas. Equivale à LTDA no Brasil."],
+      tag: "Forma Societária"
+    },
+    {
+      id: "eefd6d19-5ce5-46e9-aa26-d5227aba4761",
+      term: "LTDA",
+      descriptions: [
+        "A sociedade é o tipo de natureza jurídica em que duas ou mais pessoas se juntam para abrir uma empresa, seja dividindo igualmente ou definindo alguém que terá maior controle sobre o negócio.",
+        "No caso da Limitada (LTDA), os sócios têm responsabilidade por sua parte na cota, porém todos respondem pela integralização do capital social. É um modelo mais simples e protege o patrimônio individual dos sócios: caso a empresa acabe contraindo dívidas, os bens particulares das pessoas físicas não serão usados para quitar os débitos."
+      ],
+      tag: "Forma Societária"
+    },
+    {
+      id: "3a158451-93ca-4954-9f65-996e9c85fd3a",
+      term: "SA",
+      descriptions: [
+        "Sociedade Anônima (SA) é uma natureza jurídica na qual a participação e a responsabilidade dos sócios é definida pela quantidade de ações que possuem. Uma das características é que o patrimônio pessoal do acionista fica separado do patrimônio da empresa.",
+        "A participação e a responsabilidade de cada sócio, chamados de acionistas, está totalmente vinculada e limitada ao preço de emissão das ações que adquirir. A Sociedade Anônima é regulamentada pela Lei 6.404/76, a qual também é conhecida como Lei das Sociedades Anônimas."
+      ],
+      tag: "Forma Societária"
+    },
+    {
+      id: "3b53cdd0-a8b5-4683-aa6b-03a992385a94",
+      term: "Incentivo de Longo Prazo (ILP)",
+      descriptions: [
+        "Incentivos de longo prazo ou ILPs são benefícios que costumam ocorrer com períodos de tempo superiores a um ano. O reconhecimento é concedido aos colaboradores com recompensas que podem variar de acordo com a estratégia escolhida, estimulando o desenvolvimento dos profissionais dentro da empresa e a busca pela evolução constante.",
+        "Os objetivos gerais dos planos de incentivos de longo de prazo são: atração e retenção dos colaboradores, redução de turnover, alta motivação do time e aumento do interesse em participar das ações da companhia."
+      ],
+      tag: null,
+    },
+    {
+      id: "7868c6e1-b3fc-485a-ad99-c6df4d376f9d",
+      term: "Stock Options",
+      descriptions: ["Stock options são a oportunidade que um determinado colaborador recebe de comprar ações da empresa no futuro, sob um valor previamente acordado (preço de exercício). É importante ressaltar que ele não é obrigado a exercer a compra das ações."],
+      tag: "Incentivo de Longo Prazo (ILP)"
+    },
+    {
+      id: "5e419884-7f28-4969-9410-84ff5a036ad0",
+      term: "Restrict Stock Units (RSU)",
+      descriptions: ["Restricted Stock Units ou RSUs são uma forma de remuneração baseada em ações, usada para recompensar os funcionários de uma companhia. Essa concessão é restrita porque está sujeita a um cronograma de aquisição, e também porque é regida por outros limites de transferências ou vendas que a empresa pode impor. A concessão das ações tem regras que valem para cada caso, sendo baseadas no tempo de trabalho ou em metas de desempenho."],
+      tag: "Incentivo de Longo Prazo (ILP)"
+    },
+    {
+      id: "59641a85-42bf-49d2-a16c-4fa8a7ecaad3",
+      term: "Livros Societários",
+      descriptions: [
+        "Os livros societários ou livros sociais são responsáveis por formalizar todos os atos societários do negócio. Eles devem ser constantemente atualizados e autenticados na Junta Comercial, caso contrário, a empresa pode ter sérios problemas envolvendo disputas societárias e fiduciárias ou, em outros casos, comprometer o recebimento de aportes financeiros.",
+        "Existem diversas categorias de livros societários. Alguns livros são obrigatórios para todo o tipo de empresa S/A, enquanto outros são facultativos e dependem do tipo de Sociedade Anônima."
+      ],
+      tag: null
+    },
+    {
+      id: "2ef978b5-288b-46a2-aa0a-2aeabe9a5016",
+      term: "Mútuo Conversível",
+      descriptions: ["Um mútuo conversível é que um empréstimo com uma opção de conversão em participação societária (quotas ou ações) “embutida”. A investidora, mutuante, empresta dinheiro à empresa, mutuária, recebendo em troca uma opção de, no futuro, observadas certas condições, converter seu investimento em participação ou receber o dinheiro de volta."],
+      tag: null
+    },
+      {
+      id: "cf88dbcb-fb64-4c0a-929a-00968fa9a78e",
+      term: "Opção",
+      descriptions: ["Uma opção é uma “negociação que garante um direito futuro de opção de comprar ou vender com preço pré-determinado”."],
+      tag: null
+    },
+    {
+      id: "fa7f78b8-37a7-4375-b577-a6d44053f2f7",
+      term: "Quotas",
+      descriptions: ["As quotas ou cotas de uma empresa são a fração que cada sócio receberá do capital social da empresa, ou seja, a divisão do capital social entre os sócios. De forma simples, é o equivalente a uma ação com a diferença de que, para as empresas, utiliza-se o termo “ações”, enquanto para as sociedades anônimas é utilizado o termo “quota”."],
+      tag: null
+    },
+    {
+      id: "a77eed4c-768e-47b5-8feb-7cc13a0b7ac9",
+      term: "Rodadas de Investimento",
+      descriptions: ["Rodadas de investimentos são a principal ferramenta utilizada por startups para acelerar o crescimento. Nelas, os empreendedores apresentam seus projetos para investidores, que, se interessados, disponibilizam capital com a condição de obterem uma participação acionária no negócio. Elas não são empréstimos, os investidores estão fazendo investimentos com a finalidade de participarem nos ganhos futuros da empresa."],
+      tag: null
+    },
+    {
+      id: "e760fdd0-e420-4abb-912d-3a76f5763b0b",
+      term: "Stakeholder",
+      descriptions: ["Um stakeholder é uma parte que tem interesse em uma empresa e pode afetar ou ser afetada pelo negócio. Os principais stakeholders em uma corporação típica são seus investidores, funcionários, clientes e fornecedores."],
+      tag: null
+    },
+    {
+      id: "ecf1df32-98ca-4f6d-9b33-e52bec387d18",
+      term: "Strike Price",
+      descriptions: ["Também conhecido como preço de exercício, é o valor que deve ser pago caso a opção seja exercida."],
+      tag: null
+    },
+    {
+      id: "50bb3a7c-4425-4cbe-aae6-247f96ebf161",
+      term: "Transações",
+      descriptions: ["Uma transação é “um acordo concluído entre um comprador e um vendedor para trocar bens, serviços ou ativos financeiros em troca de dinheiro”. Ou seja, a Pessoa A paga a Pessoa B em troca de um produto ou serviço e quando elas concordam com os termos, o dinheiro é trocado pelo bem e a transação é concluída."],
+      tag: null
+    },
+    {
+      id: "00cfc505-7c9f-4bf0-a780-725b8ca12a5b",
+      term: "Vesting",
+      descriptions: [
+        "É um termo legal que diz respeito à aquisição progressiva de direitos de uma empresa. Geralmente significa ganhar o direito de exercer um pagamento, ativo ou benefício presente ou futuro.",
+        "O calendário ou cronograma de vesting estabelecido por uma empresa determina quando os funcionários podem adquirir a propriedade total do ativo."
+      ],
+      tag: null
+    },
+    {
+      id: "e715e01d-0a24-4377-bbe1-9f1a98716a81",
+      term: "Warrants",
+      descriptions: [
+        "Um warrant é um título de garantia, que permite que o beneficiário compre ou venda um ativo. É muito utilizado no mercado de commodities (produtos de origem agropecuária ou de extração mineral), mas pode fazer referência a uma ação, índice ou taxa de câmbio.",
+        "Seu valor não é dado como uma fração de uma empresa, mas mercadorias físicas que fazem parte de um estoque. De forma geral, “é um título que transfere parte da propriedade de um lote de mercadorias para o seu investidor”."
+      ],
+      tag: null
+    }
+  ]
+}
